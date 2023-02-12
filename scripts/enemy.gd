454f2ac1d726a5f5048cfaafe84bd29932ef0947
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-02-11 23:48:33 -0600

    commit

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -10,16 +10,15 @@ func _ready():
 	print(boxes)
 
 func hit():
-	print('hit')
+	#print('hit')
 	for box in boxes:
-		print('in loop')
 		if is_collided(self, box):
-			box.position = Vector3(randf_range(-7, 7), randf_range(-7, 7), start.z)
+			position = Vector3(randf_range(-7, 7), randf_range(-7, 7), start.z)
 			
 func is_collided(box1, box2):
 	print("in collided")
 	var distance = box1.position.distance_to(box2.position)
-	var combined_size = (box1.scale + box2.scale).length() + 4
+	var combined_size = 8
 	if distance <= combined_size / 2:
 		return true
 	return false
