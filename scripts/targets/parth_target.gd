5b11ba7421d253c7465ac0fa437f281372be0c17
JacobM311 <98193759+JacobM311@users.noreply.github.com>
2023-02-13 23:56:11 -0600

    push

--- scripts/targets/parth_target.gd
+++ scripts/targets/parth_target.gd
@@ -3,6 +3,7 @@ extends PathFollow3D
 @export_range(0, 1) var speed = 0.2
 
 var r = 1
+var rand = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,17 +11,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if r == 1:
-		progress_ratio += delta * speed
-		if progress_ratio >= 0.9:
-			r = 2
-
-	if r == 2:
-		if progress_ratio <= 0.1:
-			r = 1
+	if progress_ratio == 0:
+		r = 1
+	if progress_ratio == 1:
+		r = -1
 
 func _physics_process(delta):
-	progress_ratio += delta * speed * -1
+	progress_ratio += delta * speed * r 
+	progress_ratio = clamp(progress_ratio, 0, 1)
+	print(r)
 
 func _on_timer_timeout():
-	r = randi_range(1, 2)
+	rand = randi_range(1, 2)
+	if rand == 1:
+		r = -1
+
+	if rand == 2:
+		r = 1
+	
